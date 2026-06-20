2df17fb8255737efd26995c7460886f535f3fe54
jjinheeWorld <wlsgml450@naver.com>
2026-06-20 20:50:22 +0900

    feature: 로그아웃 API 연동 (#5)

--- api/auth.api.ts
+++ api/auth.api.ts
@@ -4,3 +4,7 @@ import { LoginRequest } from '@/types/auth.type';
 export const login = async (request: LoginRequest): Promise<void> => {
   await apiClient.post('/api/v1/admin/auth/login', request);
 };
+
+export const logout = async (): Promise<void> => {
+  await apiClient.post('/api/v1/admin/auth/logout');
+};
